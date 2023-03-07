953e7c08a4ed1ab0600328ff00ea67bd5bb58972
Hyunjin Choi <chg5638@gmail.com>
2023-03-07 13:14:47 +0900

    sword_man added
    animation and transform logic change

--- new_project/Assets/Scripts/Sword_Man.cs
+++ new_project/Assets/Scripts/Sword_Man.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Sword_Man : MonoBehaviour
 {
+    public GameObject objSwordMan;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        if (h > 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool("moving", true);
+        }
+        else if (h < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("moving", true);
+        }
+        else animator.SetBool("moving", false);
+        transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime);
     }
 }

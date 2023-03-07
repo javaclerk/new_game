334b3ca9c3606048d76cd23c4204be720a0c3523
Hyunjin Choi <chg5638@gmail.com>
2023-03-07 16:43:05 +0900

    sword man movemnet add and enemy add

--- new_project/Assets/Scripts/Sword_Man.cs
+++ new_project/Assets/Scripts/Sword_Man.cs
@@ -16,18 +16,24 @@ public class Sword_Man : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        if (h > 0) 
+        if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.localScale = new Vector3(-1, 1, 1);
             animator.SetBool("moving", true);
+            transform.Translate(Vector3.right * Time.deltaTime);
         }
-        else if (h < 0)
+        else if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.localScale = new Vector3(1, 1, 1);
             animator.SetBool("moving", true);
+            transform.Translate(Vector3.left * Time.deltaTime);
         }
         else animator.SetBool("moving", false);
-        transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.A) &&
+            !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            {
+                animator.SetTrigger("attack");
+            }
     }
 }
